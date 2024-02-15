84c3f2f8a143f814d016f5abc0d772f304787337
Zephyr Serret Verbist <zserret@me.com>
2024-02-15 12:36:05 +0100

    finished solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (a:i32) -> i32 {
+    if a > 40 {
+        return a ;
+    }
+    a * 2
+}
 
 // Don't modify this function!
 #[test]
